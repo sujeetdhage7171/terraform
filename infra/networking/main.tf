c3e8fdd3a9be03998534b9635fe50f3f12e53830
sudhage <Sujit.Dhage@msg-global.com>
2025-09-29 15:13:35 +0530

    Test connectivity

---
+++ infra/networking/main.tf
@@ -0,0 +1,13 @@
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+}
+
+data "aws_availability_zones" "available" {}
