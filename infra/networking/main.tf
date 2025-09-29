22e6f8319a51cbf4ce528c2f9b65e3b0e0bba802
sudhage <Sujit.Dhage@msg-global.com>
2025-09-29 15:36:18 +0530

    deploy VPC

--- infra/networking/main.tf
+++ infra/networking/main.tf
@@ -1,13 +1,30 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Simple VPC with /24 CIDR
 resource "aws_vpc" "this" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/24"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "simple-vpc"
+  }
 }
 
-resource "aws_subnet" "public" {
-  count                   = 2
+# Single subnet inside that VPC
+resource "aws_subnet" "this" {
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index)
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "simple-subnet"
+  }
 }
 
+# Just to resolve AZs
 data "aws_availability_zones" "available" {}
+
