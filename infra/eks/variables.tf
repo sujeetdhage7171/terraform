32968e6dc12d469d6c56c3e5e2de6e47814b93c8
sudhage <Sujit.Dhage@msg-global.com>
2025-10-17 14:31:22 +0530

    All var values added

--- infra/eks/variables.tf
+++ infra/eks/variables.tf
@@ -3,6 +3,7 @@
 variable "vpc_id" {
   description = "The ID of the VPC created by the networking module."
   type        = string
+  default = "vpc-0d934e4e540c75060"
 }
 
 variable "private_subnet_ids" {
@@ -19,6 +20,7 @@ variable "private_subnet_ids" {
 variable "region" {
   description = "The AWS region to deploy resources into."
   type        = string
+  default     = "us-east-1"
 }
 
 variable "eks_worker_plane" {
@@ -30,21 +32,25 @@ variable "eks_worker_plane" {
 variable "node_machine_type" {
   description = "The EC2 instance type for the EKS worker nodes."
   type        = string
+  default     = "t3.medium"
 }
 
 variable "node_count" {
   description = "The initial number of worker nodes."
   type        = number
+  default     = 2
 }
 
 variable "eks_cluster_role_arn" {
   description = "ARN of the IAM role for the EKS Control Plane."
   type        = string
+  default     = "arn:aws:iam::555488808142:role/eks_cluster_role"
 }
 
 variable "eks_node_role_arn" {
   description = "ARN of the IAM role for the EKS Worker Nodes."
   type        = string
+  default     = "arn:aws:iam::555488808142:role/eks_node_role"
 }
 
 variable "cluster_version" {
